d85f50a9343a7c1c7f27c28228a767fc36e228f9
andy311p <68938613+andy311p@users.noreply.github.com>
2020-07-29 11:00:52 +0300

    Update ocean_format.py

--- ocean_format.py
+++ ocean_format.py
@@ -5,13 +5,30 @@ import json
 import wave
 import contextlib
 
+
+# with io.open(script_path + "/1110.txt", 'r', encoding='gb2312') as fn:
+#   lines = fn.readlines()
+
+# for fname in os.listdir(script_path):
+#     blockSize = 1048576
+#     with codecs.open(script_path + fname,"r",encoding="utf-16") as sourceFile:
+#         with codecs.open(script_path + fname + "_","w",encoding="UTF-8") as targetFile:
+#             while True:
+#                 for i in sourceFile.readlines():
+#                     a = i
+#                 contents = sourceFile.read(blockSize)
+#                 if not contents:
+#                     break
+#                 targetFile.write(contents)
+
 script_path = str(pathlib.Path(__file__).parent.as_posix()) + '/ocean/Script/'
 channel_path = str(pathlib.Path(__file__).parent.as_posix()) + '/ocean/Channel'
 
 data = []
 #run over all the files in the Script path
 for fname in os.listdir(script_path):
-    file = open(script_path + fname,'r',encoding="utf8")
+    #codecs.encode(fname,encoding='utf-8', errors='strict')
+    file = open(script_path + fname,'r',encoding="gb2312")
     for i,line in enumerate(file.readlines()[0:2]):
         if i % 2 == 0:
             line = line.split()
@@ -34,4 +51,3 @@ with open('output.txt', 'w',encoding="utf-8") as outfile:
     for i in data:
         json.dump(i, outfile,ensure_ascii=False)
         outfile.write('\n')
-
